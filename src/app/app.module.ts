0b3f8e2721654660aba223913538c37595af6c57
akashgaikwad91 <akash.gaikwad545@gmail.com>
2021-02-01 02:16:41 +0530

    added signup form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
 import { MaterialModule } from './material.module';
+
+import { WelcomeComponent } from './welcome/welcome/welcome.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training/training.component';
 import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
-import { WelcomeComponent } from './welcome/welcome/welcome.component';
 
 @NgModule({
   declarations: [
@@ -25,10 +29,12 @@ import { WelcomeComponent } from './welcome/welcome/welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    BrowserModule,
+    FlexLayoutModule,
+    FormsModule,
+    MaterialModule,   
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
-    imports: [MatButtonModule,MatIconModule],
-    exports: [MatButtonModule,MatIconModule]
+    imports: [MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatIconModule, MatFormFieldModule, MatInputModule, MatNativeDateModule],
+    exports: [MatButtonModule, MatCardModule, MatCheckboxModule, MatDatepickerModule, MatIconModule, MatFormFieldModule, MatInputModule, MatNativeDateModule]
 })
 export class MaterialModule { }
