f582a58a11e6867fb25682524b9f56794da03f6b
akashgaikwad91 <akash.gaikwad545@gmail.com>
2021-03-21 02:33:51 +0530

    added active exercises

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-new-training',
@@ -6,14 +9,15 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  @Output() trainingStart=new EventEmitter<void>();
-  constructor() { }
+  exercises:Exercise[]=[]
+  constructor(private trainingSvc:TrainingService) { }
 
   ngOnInit(): void {
+    this.exercises=this.trainingSvc.getAvailableExercises()
   }
 
-  onStartTraining() {
-    this.trainingStart.emit();
+  onStartTraining(form:NgForm) {
+    this.trainingSvc.startExercise(form.value.exercise)
   }
 
 }
--- src/app/training/training/training.component.ts
+++ src/app/training/training/training.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription } from 'rxjs/Subscription'
+import { TrainingService } from '../training.service';
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.css']
 })
 export class TrainingComponent implements OnInit {
-  ongoingTraining =false;
-  constructor() { }
+  ongoingTraining = false;
+  exerciseSubscription:Subscription
+  constructor(private trainingServices:TrainingService) { }
 
   ngOnInit(): void {
+    this.exerciseSubscription=this.trainingServices.exerciseChanged.subscribe(exercise=>{
+     if(exercise){
+       this.ongoingTraining=true
+     } else{
+      this.ongoingTraining=false
+     }
+    })
   }
 
 }
